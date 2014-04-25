cba7c44774e7cb7879313acc4e75b1e60259e92e
Matt Ross <matt@mattross.io>
2014-04-25 17:46:56 -0400

    remove hello dolly actions

--- lib/custom.php
+++ lib/custom.php
@@ -3,5 +3,11 @@
  * Custom functions
  */
 
+// prevent <head> shorttags
 remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
+// disable links in comments
 remove_filter('comment_text', 'make_clickable', 9);
+
+// remove Hello dolly stuff
+remove_action( 'admin_notices', 'hello_dolly' );
+remove_action( 'admin_head', 'dolly_css' );
